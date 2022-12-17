355b6cd6c63eebf2c33a2b5903bceaa3fcaf48ad
wen.you <wen.you27@icloud.com>
2022-12-17 12:43:45 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <nuxt/>
+</template>
+
+<style >
+body{
+  background: #000;
+  color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  h1 App
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
